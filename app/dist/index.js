321d9768a1fdfe77488b74a6c5c57d1a923adb4d
Andrew Dotterer <adotterer@gmail.com>
2021-11-23 00:36:38 -0800

    body-parser"

--- app/dist/index.js
+++ app/dist/index.js
@@ -5,13 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const console_1 = require("./console");
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-const port = 8080; // default port to listen
-// define a route handler for the default home page
+const port = 8080;
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
-// start the Express server
+app.post("/", (req, res) => {
+    // colorConsoleLog(req.body)
+    console.log(req.body);
+    res.send("hi");
+});
 app.listen(port, () => {
     (0, console_1.colorConsoleLog)(`Server started at http://localhost:${port}`);
 });
--- app/index.ts
+++ app/index.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import { colorConsoleLog } from "./console";
+import bcrypt from "bcryptjs";
+import bodyParser from "body-parser";
 const app = express();
-const port = 8080; // default port to listen
+const port = 8080;
 
-// define a route handler for the default home page
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get( "/", ( req, res ) => {
     res.send("Hello world!");
 } );
-
-// start the Express server
+app.post("/", (req, res) => {
+    // colorConsoleLog(req.body)
+    console.log(req.body)
+    res.send("hi")
+})
 app.listen(port, () => {
     colorConsoleLog(`Server started at http://localhost:${port}`);
 } );
