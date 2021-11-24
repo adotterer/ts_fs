8137c7ae8a6b3258dc6a22b1dc978c5af32c0bbe
Andrew Dotterer <adotterer@gmail.com>
2021-11-23 17:09:01 -0800

    all

--- app/server.ts
+++ app/server.ts
@@ -1,37 +1,41 @@
 import express from "express";
 import { colorConsoleLog } from "./console";
+import {connectDb} from "./db"
 import bodyParser from "body-parser";
 import mongoose  from "mongoose"
 const app = express();
-const port = 8080;
-
-const connectDb = () => {
-    return mongoose.connect('mongodb://127.0.0.1:27017/whatver')
-}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get( "/", ( req, res ) => {
     res.send("Hello world!");
-} );
-app.post("/", (req, res) => {
-    // colorConsoleLog(req.body)
-    console.log(req.body)
-    res.send("hi")
-})
+});
 
 const studentSchema = new mongoose.Schema({
   name: String
 });
 
 const Student = mongoose.model('student', studentSchema)
+app.post("/student", async (req, res) => {
+  const student = await Student.create({ name: "Andrew" });
+  console.log("student --->", student);
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    res.send("hi")
+})
+
+
+
+
+
+const port = 8080;
 
 export const start = async () => {
   try {
     await connectDb().catch(e => console.log(e));
-    const student = await Student.create({ name: "Andrew" });
-    console.log("student --->", student);
     app.listen(port, () => {
         colorConsoleLog(`Server started at http://localhost:${port}`);
     });
