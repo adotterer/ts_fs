e548fa2976b26f4668c66079db2b06a0e79a6a59
adotterer <adotterer@gmail.com>
2022-01-17 15:07:20 -0800

    todo write test

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken";
+import { config } from "process";
 import { IUser } from "../resources/user/user.model"
 
 export const newToken = (user: IUser) => {
-    return jwt.sign({id: user.id}, process.env.JWT_SECRET);
+    return jwt.sign({id: user.id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
 }
