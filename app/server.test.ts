60fbc2871bd039e70a44646fc13a5809d0a658b9
adotterer <adotterer@gmail.com>
2022-01-17 17:33:09 -0800

    just don't use done

--- app/server.test.ts
+++ app/server.test.ts
@@ -2,18 +2,43 @@ import app, {TestModel}  from "./server"
 import supertest from "supertest"
 import mongoose from "mongoose"
 
-beforeEach((done) => {
-  mongoose.connect("mongodb://127.0.0.1:27017/JestDB_CBW").then(() => done())
+// const models = { TestModel };
+// const remove = (collection: any)=>
+//   new Promise<void>((resolve, reject) => {
+//     collection.remove((err?: Error) => {
+//       if (err) return reject(err)
+//       resolve()
+//     })
+//   })
+
+beforeEach(async () => {
+  // function clearDB() {
+  //   return Promise.all(_.map(mongoose.connection.collections, (c: any) => remove(c)))
+  // }
+  if (mongoose.connection.readyState === 0) {
+    try {
+      
+      await mongoose.connect("mongodb://127.0.0.1:27017/JestDB_CBW");
+      await TestModel.init()
+      // await clearDB();
+      // await Promise.all(Object.keys(models).map(name => models[name].init()))
+      } catch(e) {
+        console.error(e);
+        // throw e
+      }
+  } 
+
 });
 
-afterEach((done) => {
-  mongoose.connection.db.dropDatabase(() => {
-    mongoose.connection.close(() => done())
+afterEach(async () => {
+  await mongoose.connection.db.dropDatabase(() => {
+    mongoose.connection.close()
   });
+
 });
 
 test("GET /test", async () => {
-  const testRow = await TestModel.create({ username: "ColdBoyWinter" });
+  const testRow = await TestModel.create({ username: "CBW" });
 
   await supertest(app).get("/test")
     .expect(200)
--- app/utils/__tests__/auth.test.ts
+++ app/utils/__tests__/auth.test.ts
@@ -1,12 +1,20 @@
 import {newToken, verifyToken} from "../auth";
 import jwt from "jsonwebtoken";
+import * as dotenv from 'dotenv';
+dotenv.config();
 // import { UserModel as User } from "../resources/user/user.model";
 
 describe("Authenication:", () => {
     describe("newToken", () => {
-        const id = "123";
-        const token = newToken(id);
-        const user = jwt.verify(token, process.env.JWT_SECRET);
-        expect(user).toBe(id);
+        test("creates new jwt from user id", () => {
+            const id = "123";
+            const token = newToken(id);
+            const user = jwt.verify(token, process.env.JWT_SECRET);
+            if(typeof user !== "string" && "id" in user) {
+                expect(user.id).toBe(id)
+            } else {
+                fail("expected type JwtPayload and received type " + typeof user)
+            };
+        })
     })
 })
