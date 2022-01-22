1f92d8ca6cbad76e36d742a54813efa965b8ef7c
adotterer <adotterer@gmail.com>
2022-01-22 12:26:18 -0800

    node fetch

--- app/server.test.ts
+++ app/server.test.ts
@@ -1,22 +1,29 @@
 import app from "./server"
-import supertest from "supertest"
+import request from "supertest"
+import {CookieAccessInfo} from "cookiejar"
+import cookieParser from "cookie-parser"
+import { verifyToken }from "./utils/auth"
+import nodeFetch from "node-fetch"
 
 describe("server app", () => {
-
-  test("GET /test", async () => {
-    await supertest(app).get("/test")
-      .expect(200)
-      .then((res) => {
-        expect(res.body.msg).toBe('Hello world!')
-      })
   
+  const agent = request.agent(app);
+  test("GET /test", async () => {
+    agent.get("/test").expect(200).then((res) => {
+      expect(res.body.msg).toBe("Hello world!")
+    })
   })
 
   test("should set the XSRF Token in cookies", async () => {
-    await supertest(app).get('/api/csrf/restore')
-      .expect(201)
+    agent.get("/api/csrf/restore").expect(201)
+    const access_info = new CookieAccessInfo("/api/csrf/restore")
+    const cookie = agent.jar.getCookie("XSRF-TOKEN",access_info)
+    const tokenOk = await verifyToken(cookie.value)
+    const res = await nodeFetch("")
+    expect(tokenOk).toBe("muffins")
+    })
 
       // look at node-cookiejar
       // https://github.com/visionmedia/supertest/issues/614
-  })
+    
 })
