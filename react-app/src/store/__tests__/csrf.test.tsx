8b27ec391ccce4dded069e20155a07e2bbd9610a
adotterer <adotterer@gmail.com>
2022-01-23 10:43:53 -0800

    mock response

--- react-app/src/store/__tests__/csrf.test.tsx
+++ react-app/src/store/__tests__/csrf.test.tsx
@@ -2,19 +2,24 @@ import * as React from 'react'
 import {csrfFetch} from "../csrf"
 import {render, fireEvent, screen} from "@testing-library/react"
 import App from "../../App"
+import Response from "http"
 
+interface MockedResponse extends Response {
+    json: () => Promise
+}
 describe("csrf fetch", () => {
     test("should pass on any headers sent in options", async () => {
 
-        const windowSpy = spyOn(window, "fetch");
-
+        // const windowSpy = jest.spyOn(window, "fetch");
+        const response: MockedResponse
+        jest.spyOn(global, 'fetch').mockResolvedValue()
         await csrfFetch("/test", {
             method: "POST",
             headers: {"Content-Type": "test"},
             body: JSON.stringify({msg: "Hello world"})
         })
-        expect(windowSpy).toHaveBeenCalled();
-
+     
+        delete global.fetch;
         
     })
 })
