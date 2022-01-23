06eb121c7a2806e08e10e45d77c04b558d35cd4f
adotterer <adotterer@gmail.com>
2022-01-23 15:06:34 -0800

    set up modal test

---
+++ react-app/src/store/__tests__/modal.test.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import {useDispatch} from "react-redux";
+import {ShowModal, HideModal, SetModalCurrent} from "../modal"
+
+function TestModal(): JSX.Element {
+    return <h1>Hello from Modal</h1>
+}
+function Test(): JSX.Element {
+    const dispatch = useDispatch();
+    dispatch(SetModalCurrent(TestModal))
+
+    return (
+        <div>
+            <button
+                className="button highlight" 
+                onClick={() => dispatch(ShowModal())}
+            >
+                Click here for modal poup
+            </button>
+            <button
+                className="button no_highlight" 
+                onClick={() => dispatch(HideModal())}
+            >
+                Click here for modal to hide
+            </button>
+        </div>
+        )
+}
