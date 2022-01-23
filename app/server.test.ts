0b093e5f6eae1de8abc6dddd6b60fa615ee3e6bd
adotterer <adotterer@gmail.com>
2022-01-22 16:36:49 -0800

    word

--- app/server.test.ts
+++ app/server.test.ts
@@ -3,7 +3,7 @@ import request from "supertest"
 import {CookieAccessInfo} from "cookiejar"
 
 describe("server app", () => {
-  
+
   const agent = request.agent(app);
   test("GET /test", async () => {
     agent.get("/test").expect(200).then((res) => {
@@ -13,12 +13,12 @@ describe("server app", () => {
 
   test("should set the XSRF Token in cookies", async () => {
     await agent.get("/api/csrf/restore").expect(201)
-    const access_info = new CookieAccessInfo("/api/csrf/restore")
-    const cookie = agent.jar.getCookie("XSRF-TOKEN",access_info)
+    const accessInfo = new CookieAccessInfo("/api/csrf/restore")
+    const cookie = agent.jar.getCookie("XSRF-TOKEN",accessInfo)
     expect(cookie.value.length).toBe(36)
     })
 
       // look at node-cookiejar
       // https://github.com/visionmedia/supertest/issues/614
-    
+
 })
