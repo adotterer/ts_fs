a775252cb1c8acfa24aba684289aec4e70e74c8f
adotterer <adotterer@gmail.com>
2022-01-23 11:15:28 -0800

    done trying this

--- react-app/src/store/__tests__/csrf.test.tsx
+++ react-app/src/store/__tests__/csrf.test.tsx
@@ -1,25 +1,31 @@
 import * as React from 'react'
-import {csrfFetch} from "../csrf"
+import {csrfFetch } from "../csrf"
 import {render, fireEvent, screen} from "@testing-library/react"
 import App from "../../App"
-import Response from "http"
+/**
+ * @jest-environment jsdom
+ */
 
 interface MockedResponse extends Response {
-    json: () => Promise
+    json: () => Promise<void>
 }
+
+declare let Response: MockedResponse
 describe("csrf fetch", () => {
     test("should pass on any headers sent in options", async () => {
 
-        // const windowSpy = jest.spyOn(window, "fetch");
-        const response: MockedResponse
-        jest.spyOn(global, 'fetch').mockResolvedValue()
-        await csrfFetch("/test", {
+    
+        const options = {
             method: "POST",
             headers: {"Content-Type": "test"},
             body: JSON.stringify({msg: "Hello world"})
-        })
+        }
+    
+        
+        await csrfFetch("/test", options);
+
      
-        delete global.fetch;
+        expect(options.headers).toBe({"Content-Type": "test"})
         
     })
 })
