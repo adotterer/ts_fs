6ab7fe86abfbd287862f704d468d38001886e124
adotterer <adotterer@gmail.com>
2022-01-23 15:05:11 -0800

    modal works as expected

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Test from "./Test"
+import "./main.css";
 
 export default function App(): JSX.Element {
   return (
     <div className='bg-black h-screen w-screen'>
       <span className='font-extrabold text-3xl text-white'>
-        Hello World!
+        <Test />
       </span>
     </div>
   );
